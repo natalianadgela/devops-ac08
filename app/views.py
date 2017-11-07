2e516d58dc010c27672948c95f8db3bae8907d52
kauaramirez <30813695+kauaramirez@users.noreply.github.com>
2017-11-07 21:15:31 -0200

    Update views.py

--- app/views.py
+++ app/views.py
@@ -63,16 +63,3 @@ def cadastro_cursos(request):
         })
     )
 
-def cadastro_vestibulares(request):
-    assert isinstance(request, HttpRequest)
-    return render(
-        request,
-        'app/cadastro_vestibulares.html',
-        context_instance = RequestContext(request,
-        {
-            'title':'Cadastro de vestibulares',
-            'vestibulares': Vestibular.objects.all(),
-            'year':datetime.now().year,
-        })
-    )   
-
